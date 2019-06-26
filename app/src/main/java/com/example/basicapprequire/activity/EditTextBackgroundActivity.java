51b33454fcd174aaf01f8b976d6b049c926e5fd0
koteswarkvana <koteswarao.kvana@gmail.com>
2019-06-26 14:47:22 +0530

    Added different folders for multiple screen support in values file.

--- app/src/main/java/com/example/basicapprequire/activity/EditTextBackgroundActivity.java
+++ app/src/main/java/com/example/basicapprequire/activity/EditTextBackgroundActivity.java
@@ -5,11 +5,17 @@ import android.os.Bundle;
 
 import com.example.basicapprequire.R;
 
+/*
+* Hide title bar. We need to change style and add code
+* getSupportActionBar().hide();
+* */
 public class EditTextBackgroundActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Below line of code can remove the title bar.
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_edit_text_background);
     }
 }
